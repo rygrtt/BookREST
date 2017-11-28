a0830684eb70e96230894c7b45cc531f5f1dbfb8
rtg <rygrtt>
2017-11-27 18:22:08 -0600

    added CORS filter

---
+++ src/main/java/com/bookrest/resources/filter/CORSFilter.java
@@ -0,0 +1,26 @@
+
+// Borrowed with extreme respect from:
+// https://stackoverflow.com/questions/28065963/how-to-handle-cors-using-jax-rs-with-jersey?answertab=votes#answer-28067653
+
+package com.bookrest.resources.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request,
+                       ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
